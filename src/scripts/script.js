04971db009351cbdf5dad83f2164f92db2003677
LevKolechkov <levkolechkov@gmail.com>
2024-10-15 21:32:20 +0700

    feat: Удаление задач происходит через окно подтверждения

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -55,10 +55,9 @@ function createTaskElement(task) {
   listItem.appendChild(container);
   listItem.appendChild(createMenu(task));
 
-  deleteButton.addEventListener("click", () => {
+  deleteButton.addEventListener("click", (event) => {
     event.stopPropagation();
-    taskList.removeChild(listItem);
-    saveTasks();
+    deleteMenu(listItem);
   });
 
   container.addEventListener("click", () => {
@@ -132,4 +131,42 @@ function createMenu(task) {
   menu.appendChild(editButton);
 
   return menu;
+}
+
+function deleteMenu(listItem) {
+  const yesButton = document.createElement("button");
+  yesButton.textContent = "Yes";
+
+  const noButton = document.createElement("button");
+  noButton.textContent = "No";
+
+  const buttonsContainer = document.createElement("div");
+  buttonsContainer.className = "delete-task__buttons";
+
+  buttonsContainer.appendChild(yesButton);
+  buttonsContainer.appendChild(noButton);
+
+  const confirmText = document.createElement("h1");
+  confirmText.textContent = "Delete this task?";
+
+  const deleteContainer = document.createElement("div");
+  deleteContainer.className = "delete-task";
+  deleteContainer.appendChild(confirmText);
+  deleteContainer.appendChild(buttonsContainer);
+
+  const overlay = document.createElement("div");
+  overlay.className = "overlay";
+  overlay.appendChild(deleteContainer);
+
+  document.body.appendChild(overlay);
+
+  yesButton.addEventListener("click", () => {
+    taskList.removeChild(listItem);
+    saveTasks();
+    document.body.removeChild(overlay);
+  });
+
+  noButton.addEventListener("click", () => {
+    document.body.removeChild(overlay);
+  });
 }
